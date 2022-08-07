106e522d7a30016e0db343a3681bcbd140f7e442
M&B Code <105244471+Bountukp20@users.noreply.github.com>
2022-08-06 20:48:33 -0500

    Update views.py

--- django_auth/django_project/accounts/views.py
+++ django_auth/django_project/accounts/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import render, redirect #old codes
-from django.urls import reverse_lazy #old codes
-from django.contrib import messages #old codes
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib import messages
 from django.views import View
 from django.contrib.auth import login, authenticate
 
 from .forms import UserRegistrationForm
 from .forms import LoginForm
 
+def homepage(request):
+    return render(request, 'home.html')
+
+def library(request):
+    return render(request, 'library.html')
+
+def about(request,id):
+    return render(request, 'about.html',{'id':id})
 
 class SignUpView(View):
-    form_class = UserRegistrationForm #old codes
-    success_url = reverse_lazy("login") #old codes
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy("login")
     initial = {'key': 'value'}
-    template_name = 'registration/sign up.html' #old codes
+    template_name = 'registration/sign up.html'
 
     def get(self, request, *args, **kwargs):
         form = self.form_class(initial=self.initial)
@@ -27,13 +35,13 @@ class SignUpView(View):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
 
-            return redirect(to='/login')
+            return redirect('login')
 
         return render(request, self.template_name, {'form': form})
     
     class LogInView(View):
         form_class = LoginForm
-        success_url = reverse_lazy("home")
+        success_url = reverse_lazy("library")
         initial = {'key': 'value'}
         template_name = 'registration/login.html'
     
@@ -44,10 +52,9 @@ class SignUpView(View):
                     password = form.cleaned_data.get('password')
                     user = authenticate(email=email, password=password)
                     login(request, user)
-                    return redirect('home')
+                    return redirect("library")
 
             context = {
                     'form':form,
             }
             return render(request, "login.html", context)
-# Create your views here.
